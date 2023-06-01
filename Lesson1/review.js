bc92c55adab1cd0561d8429118606f208b75361f
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-01 16:37:12 +0500

    bootstrap tutorial

---
+++ Lesson1/review.js
@@ -0,0 +1,12 @@
+// Containers mavzusi.
+
+// Biz bu mavzuda asosan containerlarning turlari bilan tanishib chiqamiz.
+// 1. container-floud; har doim 100% width ga ega;
+// 2. container; < 576px
+// 3. container-sm; >= 576px
+// 4. container-md; >= 768px
+// 5. container-lg; >= 992px
+// 6. container-xl; >= 1200px
+// 7. container-xxl; >= 1400px
+
+// bularning hammasi sitening responsive bo'lishiga hizmat qiladi.
