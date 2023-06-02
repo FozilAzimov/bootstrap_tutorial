14a9ea6eb65d37508936c4c6d9c25128207b06ad
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-02 16:10:46 +0500

    grid basics

---
+++ Lesson2/review.js
@@ -0,0 +1,13 @@
+// Grid System.
+
+//  BU darsimizda biz grid system bilan tanishib chiqdik.
+// Bootstrapda grid 12 ga bo'lingan. row, col tushinchalari mavjud.
+// bu grid system ni Brekpoint lar bilan birga ishlatsa ham bo'ladi.
+// col-sm
+// col-md
+// col-lg
+// col-xl
+// col-xxl
+
+// grid da auto ham bor. Bu contentni width i ga qarab joy egallaydi.
+
