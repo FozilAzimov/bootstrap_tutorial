b08ff968b366024f2f60fb7207845d043a81a52b
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-03 16:27:37 +0500

    gutters

---
+++ Lesson5/review.js
@@ -0,0 +1,12 @@
+// Mavzu: Gutters
+
+// Biz gutters dan foydalanish uchun ( gx, gy, g ) larni yaxshi bilib olishimiz kerak.
+// Bundan tashqari breakpointlar bilan ham birga ishlatsa bo'ladi (g-sm-2, g-lg-5).
+// Gutters ning min qiymati 1 va max qiymati 5 xisoblanadi.
+// Gutters ni ishlatish uchun biz row div ga beradigan bo'lsak uning ichidagi colning childiga ta'sir ko'rsatadi.
+
+
+
+
+
+
