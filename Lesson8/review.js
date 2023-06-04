4733c8b6fa3933d23bfac16a049d563fe62bc71b
FozilAzimov <foziljon.azimov@yandex.ru>
2023-06-04 23:35:04 +0500

    table

---
+++ Lesson8/review.js
@@ -0,0 +1,12 @@
+// Mavzu: Table
+
+// Asosiy class lar :
+// table, table-primary, table-sm, border, border-primary, table-bordered, table-borderless, table-hover taxt-center;
+// Table parentiga beriladi: table-responsive;
+
+
+
+
+
+
+
